3a0aa3afcbea083beb410da093fde9d7d0743523
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-30 11:16:28 +0000

    abstracts go back button to its own component

--- src/app/components/go-back/go-back.component.ts
+++ src/app/components/go-back/go-back.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router"
 
 @Component({
   selector: 'app-go-back',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './go-back.component.html',
   styleUrl: './go-back.component.css'
 })
