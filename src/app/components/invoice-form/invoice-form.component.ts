5e1065b3529785e9e4e15b3d15e53e9ad6072d04
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-30 16:55:09 +0000

    changes payment term input field to a select field

--- src/app/components/invoice-form/invoice-form.component.ts
+++ src/app/components/invoice-form/invoice-form.component.ts
@@ -18,6 +18,12 @@ import { GoBackComponent } from '../go-back/go-back.component';
 export class InvoiceFormComponent implements OnInit{
 
   form!:FormGroup
+  paymentOptions: Array<{ value: number, label: string }> = [
+    { value: 1, label: 'Net 1 Day' },
+    { value: 7, label: 'Net 7 Day' },
+    { value: 14, label: 'Net 14 Day' },
+    { value: 30, label: 'Net 30 Day' },
+    ];
 
   constructor (
     private fb: FormBuilder,
