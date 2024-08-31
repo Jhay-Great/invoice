53970a622757cebb1e904fa9575d8e1e9d28a123
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-31 11:35:46 +0000

    adds setInvoiceId and displayDeleteModal methods in application service

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicationService {
 
-  constructor() { }
+  isDeleteModalActive:boolean = false;
+
+  private invoiceIdSubject$ = new BehaviorSubject<string>('');
+  invoiceId:Observable<string> = this.invoiceIdSubject$.asObservable();
+  
+
+  constructor() { 
+
+   }
+
+   setInvoiceId(id:string) {
+    this.invoiceIdSubject$.next(id);
+   }
+
+   displayDeleteModal (id:string) {
+    this.isDeleteModalActive = true;
+    this.setInvoiceId(id);
+   }
 }
