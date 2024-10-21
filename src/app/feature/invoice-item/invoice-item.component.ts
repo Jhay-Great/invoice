3dc3e1703f4cb2c05aa04a5a3d8b44d708cac56a
Jhay-Great <alemonai.jenef@gmail.com>
2024-10-21 23:38:52 +0000

    feat: dynamically displays the number of invoices available

--- src/app/feature/invoice-item/invoice-item.component.ts
+++ src/app/feature/invoice-item/invoice-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IInvoice } from '../../core/interfaces/invoice.interface';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-item',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, CurrencyPipe, ],
   templateUrl: './invoice-item.component.html',
   styleUrl: './invoice-item.component.css'
 })
