65d7789425fc287efc32697e2b32af0255056bde
Jhay-Great <alemonai.jenef@gmail.com>
2024-10-21 13:02:42 +0000

    feat: adds route paths and route declaration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/invoice-list/invoice-list.component').then(il => il.InvoiceListComponent),
+        title: 'Invoices'
+    },
+    {
+        path: '/:id',
+        loadComponent: () => import('./pages/invoice-detail/invoice-detail.component').then(d => d.InvoiceDetailComponent),
+        title: 'Invoice',
+    }
+];
