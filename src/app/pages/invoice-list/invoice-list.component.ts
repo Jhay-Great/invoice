41d1059f7990fee3f3d3bd000134c847f926f0ed
Jhay-Great <alemonai.jenef@gmail.com>
2024-10-21 14:26:55 +0000

    feat: adds initial logic in invoice list ts file

--- src/app/pages/invoice-list/invoice-list.component.ts
+++ src/app/pages/invoice-list/invoice-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-invoice-list',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.css'
 })
-export class InvoiceListComponent {
+export class InvoiceListComponent implements OnInit {
+  invoice: any[] = [];
+
+  constructor () {};
+
+  ngOnInit ():void {
+    // fetch data from service
+  }
+
 
 }
